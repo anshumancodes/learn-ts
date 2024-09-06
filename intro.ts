e9b4044202322c92937bb9c0dcbc3420edb740bb
anshumancodes <anshumanprof01@gmail.com>
2024-09-06 21:10:37 +0530

    basic variable and functions with ts

---
+++ intro.ts
@@ -0,0 +1,14 @@
+// delcaring types for indentifiers and variables
+const age :number =34
+const Name :string = "John"
+const isMarried :boolean = true
+
+
+// declaring types for functions
+
+function add(a:number,b:number):number{
+    return a+b
+}
+
+console.log(add(2,5));
+
