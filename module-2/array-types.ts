45e97015bc47be79130926937e14b3446879e6b7
anshumancodes <anshumanprof01@gmail.com>
2024-09-07 22:22:12 +0530

    array and object

---
+++ module-2/array-types.ts
@@ -0,0 +1,8 @@
+const arr: Array<number> = [1, 2, 3, 4];
+const arror: number[] = [1, 2, 3, 4];
+const mixedArray: (string | number | boolean | object)[] = [
+    "john_doe",     // string
+    25,             // number
+    true,           // boolean
+    { role: "admin" } // object
+  ];
---
+++ module-2/object-types.ts
@@ -0,0 +1,16 @@
+const you:{
+    user:string,
+    istrue:boolean
+}={
+    user:"John",
+    istrue:true
+} //assign types to object keys
+
+
+const notYou:{
+    user:"me",
+    istrue:boolean
+}={
+    user:"John", // absoult value needed
+    istrue:true
+} 
